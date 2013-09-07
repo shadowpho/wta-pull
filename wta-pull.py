a7088459b22c395f46b78c89c690c660304f66d5
george <shadowp+git@gmail.com>
2013-09-06 19:17:48 -0700

    Got everything in one piece. Works!`

--- wta-pull.py
+++ wta-pull.py
@@ -5,6 +5,7 @@ import urllib.request,sys
 
 
 keyword = ['Roundtrip', 'Elevation Gain','Highest Point','BCRT 2010']
+information = ['','','', '','','']
 
 #used with conjuction with python docs
 #simple state machine
@@ -17,11 +18,13 @@ class BestHTMLParser(HTMLParser):
     loop_forward = 0
     state = 'None'
     second_loop = 0
+    second_state_machine=0
     def handle_data(self, data):
         self.found = False
         self.loop = self.loop+1
         if(self.loop == 4):
-            print(data[:-31])
+            information[0]=data[:-31]
+            self.second_state_machine+=1
         for str_choice in keyword:
             if(str_choice in data):  #check if keyword is located in data
 #                print('found ' + str_choice + ' on ' + str(self.loop))
@@ -35,13 +38,15 @@ class BestHTMLParser(HTMLParser):
                     self.loop_forward = 3
         if(self.loop_forward == 1):
             #FOUND DATA
-            print('found ' + self.state +' ' + data)
-            #XXX
-            if(self.state is keyword[3]):
+            #print('found ' + self.state +' ' + data)
+            information[self.second_state_machine] = data
+            self.second_state_machine+=1
+
+            if(self.state is keyword[3]): #found COORDs
                 if(self.second_loop==0):
                     self.second_loop=1
                     self.loop_forward=3
-                else:
+                else:                   #found FIRST THREE guys
                     self.state='None'
                     self.loop_forward=0
             else:
@@ -50,7 +55,7 @@ class BestHTMLParser(HTMLParser):
         if(self.loop_forward!=0):
             self.loop_forward=self.loop_forward-1
 
-print('Main')
+#print('Main')
 if(len(sys.argv) != 2):
     print('Usage: ./wta_pull http://etc')
     print('Usage: ./wta_pull hike-name')
@@ -62,10 +67,12 @@ if(sys.argv[1][0:len(look_for)] == look_for):
 else:
     full_link= 'http://www.wta.org/go-hiking/hikes/' + sys.argv[1]
 my_parser = BestHTMLParser()
-print('Opening connection to ' + full_link);
+#print('Opening connection to ' + full_link);
 f = urllib.request.urlopen(full_link)
-print('Connection Open')
+#print('Connection Open')
 rec_str = f.read().decode('utf-8')
-print('Recieved data ' + str(len(rec_str)))
+#print('Recieved data ' + str(len(rec_str)))
 
 my_parser.feed(rec_str)
+
+print(information)
